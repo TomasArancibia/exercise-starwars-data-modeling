7c380d7765b599136883bc2d41985155ede0f487
TomasArancibia <101353494+TomasArancibia@users.noreply.github.com>
2024-05-13 19:45:00 +0000

    Pokemon Models

--- src/models.py
+++ src/models.py
@@ -1,32 +1,57 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Game(Base):
+    __tablename__ = 'Game'
+    id = Column(Integer, primary_key=True)
+    Name = Column(String, nullable=False, unique=True)
+    Region = Column(String, nullable=False)
+    ReleaseDate = Column(String, nullable=False)
+
+class Character(Base):
+    __tablename__ = 'Character'
+    id = Column(Integer, primary_key=True)
+    Name = Column(String, nullable=False)
+    Job = Column(String, nullable=False)
+    Gender = Column(String, nullable=False)
+    Game = Column(Integer, ForeignKey('Game.id'))
+
+class Pokemon(Base):
+    __tablename__ = 'Pokemon'
+    id = Column(Integer, primary_key=True)
+    Name = Column(String(12), nullable=False)
+    Types = Column(String, nullable=False)
+    Gender = Column(String, nullable=False)
+    FirstGame = Column(Integer, ForeignKey('Game.id'))
+    BST = Column(Integer)
+    Shiny = Column(Boolean)
+
+class Favorites(Base):
+    __tablename__ = 'Favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    FavoritePokemon = Column(Integer, ForeignKey('Pokemon.id'))
+    FavoriteGame = Column(Integer, ForeignKey('Game.id'))
+    FavoriteCharacter = Column(Integer, ForeignKey('Character.id'))
 
-    def to_dict(self):
-        return {}
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer, primary_key=True)
+    Name = Column(String(12), nullable=False)
+    Lastname = Column(String(12), nullable=False)
+    Username = Column(String(12), nullable=False, unique =True)
+    Email = Column(String(12), nullable=False, unique =True)
+    Age = Column(Integer)
+    Favorties = Column(Integer, ForeignKey('Favorites.id'))
 
 ## Draw from SQLAlchemy base
+Game.Professor = Column(Integer, ForeignKey('Character.id'))
 render_er(Base, 'diagram.png')
